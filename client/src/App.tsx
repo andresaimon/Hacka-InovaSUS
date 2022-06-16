8ee3272be8e432b7316d77c5782026fc98d97c30
Gustavo Lobo <79761328+gfLobo@users.noreply.github.com>
2022-06-16 15:13:06 -0300

    Paperbase Add

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Paperbase from './Paperbase'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <Paperbase>
+        
+      </Paperbase>
+    
+  )
+}
+
+export default App
